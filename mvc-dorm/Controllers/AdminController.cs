20c668cfd00248b36f40d114c6590b9186076e1d
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2022-03-08 13:54:47 +0300

    create Control Panel

---
+++ mvc-dorm/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_dorm.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult AdminCity()
+        {
+            return PartialView();
+        }
+        public PartialViewResult AdminUniversity()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminDorm()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminAbout()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminNews()
+        {
+            return PartialView();
+        }
+        public PartialViewResult AdminSponsor()
+        {
+            return PartialView();
+        }
+        public PartialViewResult AdminComment()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminSocialMedia()
+        {
+            return PartialView();
+        }
+    }
+}
